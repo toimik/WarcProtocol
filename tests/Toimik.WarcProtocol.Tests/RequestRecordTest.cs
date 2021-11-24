fa9b6190f61072b04143bbab629bb34804e0e635
nurhafiz <nurhafiz@hotmail.sg>
2021-11-24 09:17:45 +0800

    Refactor to make variables constant

--- tests/Toimik.WarcProtocol.Tests/RequestRecordTest.cs
+++ tests/Toimik.WarcProtocol.Tests/RequestRecordTest.cs
@@ -11,19 +11,19 @@
         {
             var now = DateTime.Now;
             var payloadTypeIdentifier = new PayloadTypeIdentifier();
-            var payload = "payload";
-            var text = $"GET / HTTP/1.1{WarcParser.CrLf}Host: www.example.com{WarcParser.CrLf}Content-Length: 7{WarcParser.CrLf}{WarcParser.CrLf}{payload}";
+            const string Payload = "payload";
+            var text = $"GET / HTTP/1.1{WarcParser.CrLf}Host: www.example.com{WarcParser.CrLf}Content-Length: 7{WarcParser.CrLf}{WarcParser.CrLf}{Payload}";
             var contentBlock = Encoding.UTF8.GetBytes(text);
             var digestFactory = new DigestFactory("sha1");
             var payloadDigest = Utils.CreateWarcDigest(digestFactory, contentBlock);
-            var contentType = "application/http;msgtype=request";
+            const string ContentType = "application/http;msgtype=request";
             var infoId = Utils.CreateId();
             var targetUri = new Uri("http://www.example.com");
             var record = new RequestRecord(
                 now,
                 payloadTypeIdentifier,
                 contentBlock,
-                contentType: contentType,
+                ContentType,
                 infoId: infoId,
                 targetUri: targetUri,
                 payloadDigest: payloadDigest);
@@ -34,10 +34,10 @@
             Assert.Equal(payloadTypeIdentifier, record.PayloadTypeIdentifier);
             Assert.Equal(contentBlock, record.ContentBlock);
             Assert.Equal(payloadDigest, record.PayloadDigest);
-            Assert.Equal(contentType, record.ContentType);
+            Assert.Equal(ContentType, record.ContentType);
             Assert.Equal(infoId, record.InfoId);
             Assert.Equal(targetUri, record.TargetUri);
-            Assert.Equal(payload, Encoding.UTF8.GetString(record.Payload));
+            Assert.Equal(Payload, Encoding.UTF8.GetString(record.Payload));
         }
     }
 }
