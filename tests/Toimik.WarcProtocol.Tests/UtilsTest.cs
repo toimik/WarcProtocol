1f3c2dfa951888d8a467d11fab4a33cc7dbc87c2
nurhafiz <nurhafiz@hotmail.sg>
2023-04-17 17:41:12 +0800

    Test null case

---
+++ tests/Toimik.WarcProtocol.Tests/UtilsTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Toimik.WarcProtocol.Tests;
+
+using System;
+using Xunit;
+
+public class UtilsTest
+{
+    [Fact]
+    public void AddBracketsToUriWithUriThatIsNull()
+    {
+        Uri? uri = null;
+        var expectedUri = Utils.AddBracketsToUri(uri);
+
+        Assert.Null(expectedUri);
+    }
+}
